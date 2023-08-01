8b71573481e403d06df95a15ba095e8ed95b13dd
Scott Buckley <s.buckley@unsw.edu.au>
2023-08-01 15:30:05 +1000

    added some scripts

---
+++ isabelle_ASpec.sh
@@ -0,0 +1,2 @@
+L4V_ARCH=RISCV64 AINVS_QUICK_AND_DIRTY=true ACCESS_QUICK_AND_DIRTY=true INFOFLOW_QUICK_AND_DIRTY=true L4V_ARCH=RISCV64 ~/repos/verification_timeprot/isabelle/bin/isabelle jedit -d ~/repos/verification_timeprot/l4v -l ASpec&
+
---
+++ master_isabelle_ASpec.sh
@@ -0,0 +1,1 @@
+L4V_ARCH=RISCV64 ~/repos/verification_master/isabelle/bin/isabelle jedit -d ~/repos/verification_master/l4v -l ASpec&
